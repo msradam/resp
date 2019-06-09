7f644a4ab6c8ad933caec550410bdf5a3107644b
Adam M. Rahman <mrahmanadam@gmail.com>
2019-06-09 09:48:40 -0400

    Yay - more healthsites !

--- services/views.py
+++ services/views.py
@@ -16,12 +16,21 @@ class List(views.APIView):
         country = request.data.pop("country")
         with open(settings.BASE_DIR + "/services/services_db.json", 'r') as jf:
             services_db = json.load(jf)
-            services_db.append(find_nearest_hospitals(country, lat, lon))
+            services_db.append(
+                [lat] + [lon] + [country] + find_nearest_hospitals(country, lat, lon))
         with open(settings.BASE_DIR + "/services/services_db.json", 'w') as jf:
             json.dump(services_db, jf)
         return Response(status=status.HTTP_200_OK)
 
     def get(self, request):
+        if request.data:
+            lat = request.data.pop("lat")
+            lon = request.data.pop("lon")
+            country = request.data.pop("country")
+        final_response = []
         with open(settings.BASE_DIR + "/services/services_db.json") as jf:
             services_db = json.load(jf)
-        return Response(services_db)
+            for entry in services_db:
+                if entry[2] == country:
+                    final_response.append(entry)
+        return Response(final_response)
