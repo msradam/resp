9d0a4682843e04489f2b15d9767be9494f4cd383
Adam M. Rahman <mrahmanadam@gmail.com>
2019-06-08 22:04:59 -0400

    Minor refactor in database structure

--- services/resources.py
+++ services/resources.py
@@ -3,62 +3,61 @@ import math
 from operator import itemgetter
 import numpy as np
 import scipy
-
+from haversine import haversine
 import osmnx as ox
 import networkx as nx
 from sklearn.neighbors import KDTree, DistanceMetric
 
-csv = 'philippines-hospitals/philippines.csv'
+csv = 'philippines.csv'
 
-def find_latlon_keys():
-	return
 
 def find_nearest_hospitals(lon_key, lat_key, orig_lat, orig_lon, csv):
-	df = pd.read_csv(csv)
-	hospitals = []
-	for i in range(len(df)):
-		hospitals.append({"lat" : df[lat_key][i], "lon" : df[lon_key][i], "pos" : i})
-	for hospital in hospitals:
-		dist = DistanceMetric.get_metric('haversine') 
-		hospital["dist"] = dist.pairwise([orig_lat, orig_lon], [hospital["lat"], hospital["lon"]])
-	hospitals = sorted(hospitals, key=itemgetter("dist"))
-	positions = {}
-	for hospital in hospitals[:3]:
-		positions[hospital["pos"]] = hospital["dist"]
-	nearest = []
-	for pos in positions.keys():
-		nearest.append({"name" : df['name'][pos], 
-						"type" : df['type'][pos], 
-						"dist" : latlon_to_km(positions[pos]),
-						"phone" : df['phone'][pos], 
-						"hours" : df['defining-hours'][pos]})
-	return nearest
+    df = pd.read_csv(csv)
+    hospitals = []
+    print(list(dict(df).keys()))
+    for i in range(len(df)):
+        hospitals.append(
+            {"lat": df[lat_key][i], "lon": df[lon_key][i], "pos": i, "type": })
+    for hospital in hospitals:
+        dist = haversine((orig_lat, orig_lon),
+                         (hospital["lat"], hospital["lon"]))
+        hospital["dist"] = dist
+    hospitals = sorted(hospitals, key=itemgetter("dist"))
+    # print(hospitals)
+    # positions = {}
+    # for hospital in hospitals[:3]:
+    #     positions[hospital["pos"]] = hospital["dist"]
+    # nearest = []
+    # for pos in positions.keys():
+    #     nearest.append({"name": df['name'][pos],
+    #                     "type": df['type'][pos],
+    #                     "dist": df["dist"][pos],
+    #                     "phone": df['phone'][pos],
+    #                     "hours": df['defining-hours'][pos]})
+    # return nearest
 
-print(find_nearest_hospitals('X', 'Y', (123.0, 10.0), csv))
+
+print(find_nearest_hospitals('X', 'Y', 123.0, 10.0, csv))
+
 
 def find_route(orig_lat, orig_lon, dest_lat, dest_lon, radius):
-	origin = (orig_lat, orig_lon)
-	destination = (dest_lat, dest_lon)
-	G = ox.graph_from_point(origin, distance=radius)
-	(nodes, _) = ox.graph_to_gdfs(G)
-	tree = KDTree(nodes[['y', 'x']], metric='euclidean')
+    origin = (orig_lat, orig_lon)
+    destination = (dest_lat, dest_lon)
+    G = ox.graph_from_point(origin, distance=radius)
+    (nodes, _) = ox.graph_to_gdfs(G)
+    tree = KDTree(nodes[['y', 'x']], metric='euclidean')
 
-	orig_idx = tree.query([origin], k=1, return_distance=False)[0]
-	dest_idx = tree.query([destination], k=1, return_distance=False)[0]
+    orig_idx = tree.query([origin], k=1, return_distance=False)[0]
+    dest_idx = tree.query([destination], k=1, return_distance=False)[0]
 
-	closest_node_to_orig = nodes.iloc[orig_idx].index.values[0]
-	closest_node_to_dest = nodes.iloc[dest_idx].index.values[0]
+    closest_node_to_orig = nodes.iloc[orig_idx].index.values[0]
+    closest_node_to_dest = nodes.iloc[dest_idx].index.values[0]
 
-	route = nx.shortest_path(G, closest_node_to_orig, closest_node_to_dest)
+    route = nx.shortest_path(G, closest_node_to_orig, closest_node_to_dest)
 
-	print(route)
-
-
+    print(route)
 
 
 """
 instead of using watson use IBM machine learning to create lrm
 """
-
-
-
