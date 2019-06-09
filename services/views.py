d588ddb57edb208932882c85f07eae4ec0b4581b
Adam M. Rahman <mrahmanadam@gmail.com>
2019-06-09 10:10:44 -0400

    Yay - more healthsites !

--- services/views.py
+++ services/views.py
@@ -11,8 +11,8 @@ from .resources import *
 
 class List(views.APIView):
     def post(self, request):
-        lat = request.data.pop("lat")
-        lon = request.data.pop("lon")
+        lat = float(request.data.pop("lat"))
+        lon = float(request.data.pop("lon"))
         country = request.data.pop("country")
         with open(settings.BASE_DIR + "/services/services_db.json", 'r') as jf:
             services_db = json.load(jf)
@@ -28,14 +28,16 @@ class List(views.APIView):
         return Response(status=status.HTTP_200_OK)
 
     def get(self, request):
-        if request.data:
-            lat = request.data.pop("lat")
-            lon = request.data.pop("lon")
-            country = request.data.pop("country")
-        final_response = []
         with open(settings.BASE_DIR + "/services/services_db.json") as jf:
             services_db = json.load(jf)
+        if request.data:
+            lat = float(request.data.pop("lat"))
+            lon = float(request.data.pop("lat"))
+            country = request.data.pop("country")
+            final_response = []
             for entry in services_db:
                 if entry["lat"] == lat and entry["lon"] == lon and entry["country"] == country:
                     final_response.append(entry)
-        return Response(final_response)
+            return Response(final_response)
+        else:
+            return Response(services_db)
