3d92c26c179d1f251f23cadfd6272fbb7a69d79a
Adam Rahman <mrahmanadam@gmail.com>
2019-09-02 20:36:37 -0400

    Added healthsites API requests to backend; included frontend display for services

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,18 +1,33 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-const healthsites_url = 'https://healthsites.io/api/v1/healthsites/search?search_type=facility&name=bandung&format=json'
-
+const baseUrl = 'https://resp-angelhack.herokuapp.com/api/services'
 
 const Services = () => {
     const [services, setServices] = useState([])
-    console.log(axios.get(healthsites_url))
-    // useEffect( () => {
-    //     axios.get(baseUrl).then(response.data => setSurvivors(response.data))}, []
-    // )
+    useEffect( () => {
+        axios.get(baseUrl, { params:  {'location': 'Phillipines' }}).then(response => setServices(response.data))}, []
+    )
+
+    const servicesCards = () => {
+        return(
+            services.map( (service) => 
+            <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">{services.name}</h4>
+                <h6 class="card-subtitle mb-2 text-muted">Facility Type: {services.type} </h6>
+            </div>
+            </div>
+        )
+        )
+    }
 
     return(
-       services.map( (service) => <div className='card'> service </div>)
+        <div className='services'>
+            <div className='col'>
+            {servicesCards()}
+            </div>  
+            </div>
     )
 }
 
-export default Services;
+export default Services
--- server.js
+++ server.js
@@ -1,42 +1,53 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParsers = require('body-parser')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParsers = require("body-parser");
+const axios = require("axios");
 
-app.use(cors())
-app.use(bodyParsers.json())
-app.use(express.static('build'))
+console.log();
 
+app.use(cors());
+app.use(bodyParsers.json());
+app.use(express.static("build"));
 
-const Survivor = require('./models/survivor')
+const Survivor = require("./models/survivor");
 
-app.get('/api/survivors', (req, res) =>
-	{Survivor.find({}).then(survivors => {
-	    console.log(survivors);
-	    res.json(survivors.map(survivor => survivor.toJSON()))
-	});
-	});
+app.get("/api/services", (req, res) => {
+  const location = req.body.location;
+  axios
+    .get("https://healthsites.io/api/v1/healthsites/search", {
+      params: { search_type: "facility", name: `${location}`, format: "json" }
+    })
+    .then(response => res.json(response.data));
+});
 
+app.get("/api/survivors", (req, res) => {
+  Survivor.find({}).then(survivors => {
+    console.log(survivors);
+    res.json(survivors.map(survivor => survivor.toJSON()));
+  });
+});
 
-app.post('/api/survivors', (req, res) =>
-	 {
-	  const survey_answers = req.body.form_response.answers
-	  const survivor = new Survivor({
-		first_name: survey_answers[0].text,
-                family_name: survey_answers[1].text,
-		age: survey_answers[2].number,
-                recent_experience: survey_answers[3].text,
-                needed_services: survey_answers[4].text,
-                well_being: survey_answers[5].text,
-		friends_relatives: survey_answers[6].text,})
+app.post("/api/survivors", (req, res) => {
+  const survey_answers = req.body.form_response.answers;
+  const survivor = new Survivor({
+    first_name: survey_answers[0].text,
+    family_name: survey_answers[1].text,
+    age: survey_answers[2].number,
+    recent_experience: survey_answers[3].text,
+    needed_services: survey_answers[4].text,
+    well_being: survey_answers[5].text,
+    friends_relatives: survey_answers[6].text
+  });
 
-	  survivor.save().then(savedSurvivor => {
-	      res.json(savedSurvivor.toJSON())
-	  })
-	 })
-	
-const PORT = process.env.PORT
+  survivor.save().then(savedSurvivor => {
+    res.json(savedSurvivor.toJSON());
+  });
+});
+
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
